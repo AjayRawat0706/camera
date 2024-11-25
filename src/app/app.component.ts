80bc21394957f124044e5bcac5bb1c4adff05bc9
Sagar Pandey <Sagar.Pandey@acsinfotech.com>
2024-11-25 15:12:05 +0530

    "hikdh"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { response } from 'express';
+import {WebcamImage, WebcamModule} from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,WebcamModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cameraPractice';
+  permissionStatus:string='';
+  camData:any='';
+  capturedImage:any='';
+  trigger:Subject<void>=new Subject();
+  get $trigger():Observable<void>{
+    return this.trigger.asObservable();
+  }
+  checkPermission=()=>{
+    navigator.mediaDevices.getUserMedia({video:{width:500,height:500}}).then((response)=>{
+      this.permissionStatus='Allowed';
+      this.camData=response;
+      console.log(this.camData);
+    }).catch(errr=>{
+      this.permissionStatus='Denied';
+      console.log(this.permissionStatus);
+    })
+  }
+
+  capture=(event:WebcamImage)=>{
+    console.log("event",event);
+    this.capturedImage=event.imageAsDataUrl;
+  }
+  captureImage=()=>{
+     this.trigger.next();
+  }
 }
